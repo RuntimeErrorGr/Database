7bae6c51be0c295f0c3a0416e5e2c4339139ad03
Andrei Liviu Lăbău Cristea <80123184+RuntimeErrorGr@users.noreply.github.com>
2021-08-05 13:47:30 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,10 @@
 
 int main() {
 	char comanda[MAX_CMD_LEN];			
-	scanf("%s", comanda);					// citire prima comanda
+	scanf("%s", comanda);				// citire prima comanda
 	char nameDB[MAX_DB_NAME_LEN];
 	if(strcmp(comanda, "INIT_DB") == 0) {
-		scanf("%s", nameDB);				// citire nume baza de date
+		scanf("%s", nameDB);			// citire nume baza de date
 	}
 	t_db* dataBase = INIT_DB(nameDB);		// initializare baza de date
 	if(dataBase == NULL) {
@@ -19,9 +19,9 @@ int main() {
 	}
 
 	while (read) { 							
-		char comanda2[MAX_CMD_LEN];			// comanda CMD
+		char comanda2[MAX_CMD_LEN];		// comanda CMD
 		char nameTable[MAX_TABLE_NAME_LEN];	// nume tabel
-		char nameType[7];					// tipul de date
+		char nameType[7];			// tipul de date
 		scanf("%s", comanda2);
 		if(strcmp(comanda2, "CREATE") == 0) {
 			scanf("%s", nameTable);			// citire nume tabel
@@ -31,14 +31,14 @@ int main() {
 		if(strcmp(comanda2, "DELETE_DB") == 0) {	// stergere baza de date 
 			free(dataBase->tables);					
 			dataBase = NULL;
-			break;									// opreste executie program
+			break;							// opreste executie program
 		}
 		if(strcmp(comanda2, "PRINT_DB") == 0) {
 			PRINT_DB(*dataBase);					// afisarea bazei de date
 		}
 		if(strcmp(comanda2, "PRINT") == 0) {
 			scanf("%s", nameTable);					// citire nume tabel
-			PRINT(*dataBase, nameTable);			// afisare tabel
+			PRINT(*dataBase, nameTable);				// afisare tabel
 		}
 		if(strcmp(comanda2, "ADD") == 0) {
 			scanf("%s", nameTable);					// citire nume tabel
@@ -51,19 +51,19 @@ int main() {
 		if(strcmp(comanda2, "DELETE") == 0) {
 			char comenzi[4][BUF_SIZE];				
 			char buffer[BUF_SIZE];
-			int nrArgs = 0;							// numara argumentele date
-			while(!fgets(buffer, BUF_SIZE, stdin));	// citire buffer comenzi
+			int nrArgs = 0;						// numara argumentele date
+			while(!fgets(buffer, BUF_SIZE, stdin));			// citire buffer comenzi
 			char* p;
-			p = strtok(buffer, " \n");			// impart bufferul in tokens
-			while(p) { 							// cat timp am un token
-				strcpy(comenzi[nrArgs], p);		// copiez token intr-un vector
-				nrArgs++;						// incrementez nr. de comenzi
-				p = strtok(NULL, " \n");		// trec la urmatorul token
+			p = strtok(buffer, " \n");				// impart bufferul in tokens
+			while(p) { 						// cat timp am un token
+				strcpy(comenzi[nrArgs], p);			// copiez token intr-un vector
+				nrArgs++;					// incrementez nr. de comenzi
+				p = strtok(NULL, " \n");			// trec la urmatorul token
 			}
 			if(nrArgs == 1) {					// daca functia are 1 argument
-				DELETE(dataBase, comenzi[0]);	// apel stergere tabel
+				DELETE(dataBase, comenzi[0]);			// apel stergere tabel
 			}
-			else {								// daca functia are 4 argumente
+			else {							// daca functia are 4 argumente
 				char* auxS = malloc(BUF_SIZE * sizeof(char)); 	// string aux.
 				if(auxS == NULL) {							 	
 					break;										
@@ -73,7 +73,7 @@ int main() {
 				if(spaces == NULL) {
 					break;
 				}
-				strcat(spaces, GetSpaces(auxS)); 	// string de padding ' '
+				strcat(spaces, GetSpaces(auxS)); 		// string de padding ' '
 				strcat(auxS, spaces);				// adaug padding la string
 				DELETESELECT(dataBase, comenzi[0], auxS, comenzi[2],
 				comenzi[3], comenzi[1]);			// apel stergere selectiva
@@ -85,15 +85,15 @@ int main() {
 			char comenzi[4][BUF_SIZE];
 			char buffer[BUF_SIZE];
 			int nrArgs = 0;							// numara argumentele date
-			while(!fgets(buffer, BUF_SIZE, stdin)); // citire buffer comenzi
+			while(!fgets(buffer, BUF_SIZE, stdin)); 			// citire buffer comenzi
 			char* p;
-			p = strtok(buffer, " \n");			// impart bufferul in tokens
+			p = strtok(buffer, " \n");					// impart bufferul in tokens
 			while(p) {							// cat timp am un token
-				strcpy(comenzi[nrArgs], p); 	// copiez token intr-un vector
+				strcpy(comenzi[nrArgs], p); 				// copiez token intr-un vector
 				nrArgs++;						// incrementez nr. de comenzi
-				p = strtok(NULL, " \n");		// trec la urmatorul token
+				p = strtok(NULL, " \n");				// trec la urmatorul token
 			}
-			char* auxS = malloc(BUF_SIZE * sizeof(char));	// string aux.
+			char* auxS = malloc(BUF_SIZE * sizeof(char));			// string aux.
 			if(auxS == NULL) {
 				break;
 			}
@@ -102,9 +102,9 @@ int main() {
 			if(spaces == NULL) {
 				break;
 			}
-			strcat(spaces, GetSpaces(auxS));		// string de padding ' '
+			strcat(spaces, GetSpaces(auxS));			// string de padding ' '
 			strcat(auxS, spaces);					// adaug padding la string
-			SEARCH(*dataBase, comenzi[0], auxS,		// apel cautare selectiva
+			SEARCH(*dataBase, comenzi[0], auxS,			// apel cautare selectiva
 			comenzi[2], comenzi[3], comenzi[1]);
 			free(spaces);
 			free(auxS);
